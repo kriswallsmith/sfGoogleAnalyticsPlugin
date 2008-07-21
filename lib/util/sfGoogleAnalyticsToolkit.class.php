68d9e40fda44a4403cc2781d80ce28163fd23738
Kris.Wallsmith <Kris.Wallsmith@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-07-21 21:32:46 +0000

    sfGoogleAnalyticsPlugin: bugfix to compat logging logic

--- lib/util/sfGoogleAnalyticsToolkit.class.php
+++ lib/util/sfGoogleAnalyticsToolkit.class.php
@@ -13,20 +13,22 @@ class sfGoogleAnalyticsToolkit
   /**
    * Log a message.
    * 
-   * @param   mixed $subject
-   * @param   string $message
-   * @param   string $priority
+   * @param   mixed   $subject
+   * @param   string  $message
+   * @param   string  $priority
    */
-  static public function logMessage($subject, $message, $priority = 'info')
+  static public function logMessage($subject, $message, $priority = null)
   {
+    $priority = strtoupper($priority);
+    
     if (class_exists('ProjectConfiguration'))
     {
-      ProjectConfiguration::getActive()->getEventDispatcher()->notify(new sfEvent($subject, 'application.log', array($message, 'priority' => $priority)));
+      ProjectConfiguration::getActive()->getEventDispatcher()->notify(new sfEvent($subject, 'application.log', array($message, 'priority' => $priority ? constant('sfLogger::'.$priority) : sfLogger::INFO)));
     }
     else
     {
       $message = sprintf('{%s} %s', is_object($subject) ? get_class($subject) : $subject, $message);
-      sfContext::getInstance()->getLogger()->log($message, constant('SF_LOG_'.strtoupper($priority)));
+      sfContext::getInstance()->getLogger()->log($message, constant('SF_LOG_'.$priority));
     }
   }
 }
